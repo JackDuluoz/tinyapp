29e7bc842dd6404cb5e9ca080846b78130c61279
Michael <greenmr95@hotmail.com>
2022-11-29 03:37:25 +0000

    added /hello path

--- express_server.js
+++ express_server.js
@@ -1,20 +1,24 @@
-const express = require("express")
-const app = express()
-const PORT = 8080
+const express = require("express");
+const app = express();
+const PORT = 8080;
 
 const urlDatabase = {
   "Chess": "http://www.chess.com/home",
   "BBC": "http://www.bbc.com/"
-}
+};
 
 app.get("/", (request, response) => {
-  response.send("Welome to the page!")
-})
+  response.send("Welome to the page!");
+});
 
 app.get("/urls.json", (request, response) => {
-  response.json(urlDatabase)
-})
+  response.json(urlDatabase);
+});
+
+app.get("/hello", (request, response) => {
+  response.send("<html><body>No More Hello <b>World, K?</b></body></html>\n");
+});
 
 app.listen(PORT, () => {
-  console.log(`New app listening on port ${PORT}:`)
-})
+  console.log(`New app listening on port ${PORT}:`);
+});
