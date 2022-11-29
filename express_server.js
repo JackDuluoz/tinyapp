f9b5992fcc9bb1e696b0a039a8ab368e810fb921
Michael <greenmr95@hotmail.com>
2022-11-29 01:39:06 +0000

    express server created

---
+++ express_server.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app = express()
+const PORT = 8080
+
+const urlDatabase = {
+  "Chess": "http://www.chess.com/home",
+  "BBC": "http://www.bbc.com/"
+}
+
+app.get("/", (request, response) => {
+  response.send("Welome to the page!")
+})
+
+app.listen(PORT, () => {
+  console.log(`New app listening on port ${PORT}:`)
+})
