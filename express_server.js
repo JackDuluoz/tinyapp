8ffe7973cbc8b950a88340733c9a55394d3ef5f8
Michael <greenmr95@hotmail.com>
2022-11-29 07:52:28 +0000

    added POST input to create new url page

--- express_server.js
+++ express_server.js
@@ -4,14 +4,29 @@ const PORT = 8080;
 
 app.set("view engine", "ejs")
 
+function generateRandomString() {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+  let randomString = ""
+  while (randomString.length < 6) {
+    randomString += characters[Math.floor(Math.random() * characters.length)]
+  }
+  return randomString
+}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/urls", (request, response) => {
   const templateVars = { urls: urlDatabase };
   response.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (request, response) => {
@@ -33,4 +48,9 @@ app.get("/hello", (request, response) => {
 
 app.listen(PORT, () => {
   console.log(`New app listening on port ${PORT}:`);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
