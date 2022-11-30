49181ba3ef11cc8565c7bdf7769ec99df9e1a8c5
Michael <greenmr95@hotmail.com>
2022-11-30 17:01:59 +0000

    incorporated cookies, login and logout features and tested

--- express_server.js
+++ express_server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cookieParser = require('cookie-parser')
 const app = express();
 const PORT = 8080;
 
 app.set("view engine", "ejs")
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
 
 function generateRandomString() {
   const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
@@ -18,23 +21,22 @@ const urlDatabase = {
   '9sm5xK': "http://www.google.com"
 };
 
-app.use(express.urlencoded({ extended: true }));
-
 app.get("/", (req, res) => {
   res.redirect("/urls");
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"]};
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
@@ -62,6 +64,24 @@ app.post("/urls/:id", (req, res) => {
   urlDatabase[shortURL] = req.body.longURL
   res.redirect('/urls')
 })
+
+app.post('/login', (req, res) => {
+  const username = req.body.username
+  res.cookie("username", username)
+  console.log(`Added Cookie: ${username}`)
+  res.redirect('/urls');
+})
+
+app.post('/logout', (req, res) => {
+  const username = req.cookies.username
+  res.clearCookie("username", username)
+  console.log(`Deleted Cookie: ${username}`)
+  res.redirect('/urls');
+})
+
+// app.get('/login', (req, res) => {
+//   res.redirect('/urls')
+// })
 
 app.listen(PORT, () => {
   console.log(`New app listening on port ${PORT}:`);
